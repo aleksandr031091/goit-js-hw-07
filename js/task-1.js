1ff73d0ed36058c5f3f77c8d0572780f1a664600
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-02-26 22:47:16 +0200

    Дз JS
    Создает структуру,Добавляет два файла JS с уже готовым решением ДЗ № 1 № 2

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+
+const categoriesE = document.querySelectorAll('.item')
+
+console.log(`в списке ${categoriesE.length} категории`)
+
+const categoriesElArr = [...categoriesE]
+    .map(item => `${item.children[0].textContent} ${item.children[1].children.length}-категории`)
+    .join(',')
+
+console.log(categoriesElArr)
---
+++ js/task-2.js
@@ -0,0 +1,52 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+const ingredientsArr = ingredients.map(el => {
+
+  const intagrationLink = document.createElement('li')
+    
+        intagrationLink.textContent = el;
+        
+        return intagrationLink;
+});
+
+const intagrationList = document.querySelector('#ingredients')
+
+intagrationList.append(...ingredientsArr)
+
+console.log(intagrationList);
+
+
+// const makeLi = options => {
+    
+//     return options.map(option => {
+        
+//         const intagrationLink = document.createElement('li')
+    
+//         intagrationLink.textContent = option;
+
+//         return intagrationLink;
+//     })
+  
+// };
+
+// const arrLi = makeLi(ingredients);
+
+// intagrationList.append(...arrLi)
+
+// console.log(intagrationList);
+// //  console.log(arrLi);
+
+
+
+
+
+
+
+
+
