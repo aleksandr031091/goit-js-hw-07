eda6993a234d86a4e3011721381ce109bde861dc
Aleksandr <prostotak4tobrabotalo@gmail.com>
2021-03-01 16:41:56 +0200

    Дз №1 решение задачи№6

--- js/task-6.js
+++ js/task-6.js
@@ -4,3 +4,38 @@
 // Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
 // Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid.
+
+
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+const refs = {
+
+    input: document.querySelector('#validation-input'),
+
+};
+
+const validInput = () => { 
+    if (refs.input.value.length === Number(refs.input.dataset.length)) { 
+
+        refs.input.classList.add('valid')
+        refs.input.classList.remove('invalid')
+
+    } else { 
+        
+        refs.input.classList.add('invalid')
+         refs.input.classList.remove('valid')
+    }
+
+};
+
+refs.input.addEventListener('blur',validInput);
